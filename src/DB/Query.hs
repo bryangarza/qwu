dad91f0c5591a3ac6d81554060dd59d6af7d8d68
Bryan Garza <brygarza@gmail.com>
2015-09-23 15:00:43 -0700

    Create first actual query: posts by user id (uuid)

--- src/DB/Query.hs
+++ src/DB/Query.hs
@@ -1,16 +1,42 @@
+{-# LANGUAGE Arrows           #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE NamedFieldPuns   #-}
+
 module DB.Query where
 
+import DB.Connect
 import DB.Table.Post as P
 
+import qualified Data.UUID as U
+import Control.Arrow (returnA, (<<<))
 import Data.Profunctor.Product.Default (Default)
+import Database.PostgreSQL.Simple (Connection)
+import Opaleye.PGTypes (pgUUID)
 import Opaleye ( Query
+               , restrict
+               , (.==)
                , queryTable
+               , runQuery
                , showSqlForPostgres
                , Unpackspec )
 
+
 postQuery :: Query P.ColumnR
 postQuery = queryTable P.table
+
+postByAccountId :: U.UUID -> Query P.ColumnR
+postByAccountId id_ = proc () -> do
+  row@Post{accountId} <- postQuery -< ()
+  restrict -< accountId .== pgUUID id_
+  returnA -< row
+
+runPostByAccountId :: IO [P.Post]
+runPostByAccountId =
+  do
+    conn <- pConnect
+    runPostByAccountId' conn (postByAccountId U.nil)
+      where runPostByAccountId' :: Connection -> Query P.ColumnR -> IO [P.Post]
+            runPostByAccountId' = runQuery
 
 printSql :: Default Unpackspec a a => Query a -> IO ()
 printSql = putStrLn . showSqlForPostgres
